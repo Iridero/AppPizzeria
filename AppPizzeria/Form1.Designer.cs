d2f213dbeefed6af97c7ad9306d30bfc62a506e2
Iridero <45569252+Iridero@users.noreply.github.com>
2021-11-16 13:36:18 -0600

    Agregar archivos de proyecto.

---
+++ AppPizzeria/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace AppPizzeria
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTamaño = new System.Windows.Forms.TextBox();
+            this.chkQuesoExtra = new System.Windows.Forms.CheckBox();
+            this.chkMasaDelgada = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstIngredientes = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIngrediente = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(12, 106);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tamaño";
+            // 
+            // txtTamaño
+            // 
+            this.txtTamaño.Location = new System.Drawing.Point(12, 27);
+            this.txtTamaño.Name = "txtTamaño";
+            this.txtTamaño.Size = new System.Drawing.Size(100, 23);
+            this.txtTamaño.TabIndex = 2;
+            // 
+            // chkQuesoExtra
+            // 
+            this.chkQuesoExtra.AutoSize = true;
+            this.chkQuesoExtra.Location = new System.Drawing.Point(12, 56);
+            this.chkQuesoExtra.Name = "chkQuesoExtra";
+            this.chkQuesoExtra.Size = new System.Drawing.Size(118, 19);
+            this.chkQuesoExtra.TabIndex = 3;
+            this.chkQuesoExtra.Text = "Tiene queso extra";
+            this.chkQuesoExtra.UseVisualStyleBackColor = true;
+            // 
+            // chkMasaDelgada
+            // 
+            this.chkMasaDelgada.AutoSize = true;
+            this.chkMasaDelgada.Location = new System.Drawing.Point(12, 81);
+            this.chkMasaDelgada.Name = "chkMasaDelgada";
+            this.chkMasaDelgada.Size = new System.Drawing.Size(113, 19);
+            this.chkMasaDelgada.TabIndex = 4;
+            this.chkMasaDelgada.Text = "Ès masa delgada";
+            this.chkMasaDelgada.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ingredientes";
+            // 
+            // lstIngredientes
+            // 
+            this.lstIngredientes.FormattingEnabled = true;
+            this.lstIngredientes.ItemHeight = 15;
+            this.lstIngredientes.Location = new System.Drawing.Point(156, 35);
+            this.lstIngredientes.Name = "lstIngredientes";
+            this.lstIngredientes.Size = new System.Drawing.Size(120, 94);
+            this.lstIngredientes.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(310, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ingrediente";
+            // 
+            // txtIngrediente
+            // 
+            this.txtIngrediente.Location = new System.Drawing.Point(310, 27);
+            this.txtIngrediente.Name = "txtIngrediente";
+            this.txtIngrediente.Size = new System.Drawing.Size(100, 23);
+            this.txtIngrediente.TabIndex = 8;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(310, 56);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(100, 23);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(310, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(310, 106);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.ReadOnly = true;
+            this.txtPrecio.Size = new System.Drawing.Size(100, 23);
+            this.txtPrecio.TabIndex = 11;
+            this.txtPrecio.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 209);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtIngrediente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstIngredientes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chkMasaDelgada);
+            this.Controls.Add(this.chkQuesoExtra);
+            this.Controls.Add(this.txtTamaño);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCrear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTamaño;
+        private System.Windows.Forms.CheckBox chkQuesoExtra;
+        private System.Windows.Forms.CheckBox chkMasaDelgada;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstIngredientes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIngrediente;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrecio;
+    }
+}
+
---
+++ AppPizzeria/Pizza.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPizzeria
+{
+    class Pizza
+    {
+
+        private string tamaño;
+        /// <summary>
+        /// Obtiene o establece el tamaño de la pizza.
+        /// El tamaño puede ser Individual, Mediana o
+        /// Familiar
+        /// </summary>
+        public string Tamaño
+        {
+            set // Establecer
+            {
+                tamaño = value;
+            }
+            get // Obtener
+            {
+                return tamaño;
+            }
+        }
+        private string[] ingredientes = new string[7];
+        /// <summary>
+        /// Obtiene la lista de ingredientes adicionales.
+        /// </summary>
+        public string[] Ingredientes
+        {
+            get
+            {
+                return ingredientes;
+            }
+        }
+        int numIngredientes = 0;
+        public void AgregarIngrediente(string ingrediente)
+        {
+            ingredientes[numIngredientes] = ingrediente;
+            numIngredientes++;
+        }
+        private bool tieneQuesoExtra;
+        
+        /// <summary>
+        /// Obtiene o establece si la pizza tiene
+        /// queso extra.
+        /// </summary>
+        public bool TieneQuesoExtra
+        {
+            set
+            {
+                tieneQuesoExtra = value;
+            }
+            get
+            {
+                return tieneQuesoExtra;
+            }
+        }
+        private bool esMasaDelgada;
+        /// <summary>
+        /// Obtiene o establece si la pizza es de
+        /// masa delgada
+        /// </summary>
+        public bool EsMasaDelgada
+        {
+            set
+            {
+                esMasaDelgada = value;
+            }
+            get
+            {
+                return esMasaDelgada;
+            }
+        }
+        private decimal costo;
+        /// <summary>
+        /// Obtiene el costo de la pizza
+        /// </summary>
+        public decimal Costo
+        {
+            get
+            {
+                switch (tamaño.ToUpper())
+                {
+                    case "INDIVIDUAL":
+                        costo = 49;
+                        break;
+                    case "MEDIANA":
+                        costo = 79;
+                        break;
+                    default:
+                        costo = 149;
+                        break;
+                }
+                costo += (numIngredientes > 1) ? (numIngredientes - 1) * 20 : 0;
+                costo += (tieneQuesoExtra) ? 15 : 0;
+                costo += (esMasaDelgada) ? 40 : 0;
+                return costo;
+            }
+        }
+
+    }
+}
